d126dd348d9d510df0ad5be98fa7042e9a5bd1b4
Youssoufou Laouali <youssoufoulaoualibaraou@gmail.com>
2025-03-19 11:56:26 +0000

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,47 @@
+import { LoggingInterceptor } from './logger/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigService } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/all-exception.filter';
+import { APP_FILTER } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { configurationSchema } from './config/config.validation';
+
+@Module({
+  imports: [PrismaModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => [
+        {
+          ttl: configService.get<number>('throttle.ttl', 60),
+          limit: configService.get<number>('throttle.limit', 10),
+        },
+      ],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+            validationSchema: configurationSchema,
+
+    }),
+],
+  controllers: [AppController],
+  providers: [ {
+  provide: APP_INTERCEPTOR,
+  useClass: LoggingInterceptor,
+},
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },AppService],
+})
+export class AppModule {}
